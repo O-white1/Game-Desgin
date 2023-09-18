418ecf1ea72f1e1a3f5d1aaf3d57c5c7e96e1712
O-white1 <white.o3@stu.janesville.k12.wi.us>
2023-09-18 12:50:35 -0500

    9/18
    all is done besides 93al.

--- prog_93a.gd
+++ prog_93a.gd
@@ -18,10 +18,12 @@ func _on_btn_exit_pressed():
 func _on_btn_calc_pressed():
 	var killowats = float($txt_killowats.text)
 	var baserate = killowats * 0.0475
-	var surcharge = baserate * .1
+	var surcharge = baserate * 0.1
 	var citytax = baserate * 0.3
 	var total = baserate + surcharge + citytax
 	$amt_owed.text = "baserate: $%.2f" %(baserate) + "surcharge: $%.2f" %(surcharge) + "citytax: $%.2f" %(citytax)
-	var lt = total * 1.04
+	var late_fee = total * 1.04
+	$amt_owed2.text = late_fee
+	
 	
 	
