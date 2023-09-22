54a21a3c6bbeb87f0262afd19debf4c64c7513ca
O-white1 <white.o3@stu.janesville.k12.wi.us>
2023-09-22 12:47:11 -0500

    9/22/23
    weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- LP4-3.gd
+++ LP4-3.gd
@@ -16,13 +16,28 @@ func _on_btn_calc_pressed():
 	var dozens = floor(eggs / 12)
 	var remainder = eggs % 12
 	var price = 0.0
-	if dozens < 0 and dozens > 4:
+	
+	if dozens > 0 and dozens <= 4:
 		price = .50
-	elif dozens > 4 and dozens < 6:
+	elif dozens > 4 and dozens <= 6:
 		price = .45
-	elif dozens > 6 and dozens < 11:
+	elif dozens > 6 and dozens <= 11:
 		price = 0.40
 	elif dozens > 11:
 		price = .35
-		
+	var totalcost = (price * dozens) + (remainder * (price/12))
+	$output.text = "price is %.2f" % totalcost + "\nprice per egg: $%.2f" % price
 	
+	
+	
+
+
+func _on_btn_clear_pressed():
+	$output.text = ""
+	$txteggs.text = ""
+	
+	
+
+
+func _on_btn_exit_pressed():
+	get_tree().quit()
