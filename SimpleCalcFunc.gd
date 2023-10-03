1e95eb8acc7ba67be55f3cc4c3f78b6bb96586e0
O-white1 <white.o3@stu.janesville.k12.wi.us>
2023-10-03 12:51:17 -0500

    10/3 gd final

---
+++ SimpleCalcFunc.gd
@@ -0,0 +1,8 @@
+extends Control
+var num1 = int($txt_num1.text)
+var num2 = int($txt_num2.text)
+func add(): num1 + num2
+func sub(): num1 - num2
+func mul(): num1 * num2
+func div(): num1 / num2
+func power(): num1
