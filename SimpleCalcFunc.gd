11b9518033c13e62fa3517885815a2bd8be9b1ad
O-white1 <white.o3@stu.janesville.k12.wi.us>
2023-10-05 11:09:21 -0500

    10/5

--- SimpleCalcFunc.gd
+++ SimpleCalcFunc.gd
@@ -4,12 +4,6 @@ var num2 = 0
 func getnums():
 	num1 = int($txt_num1.text)
 	num2 = int($txt_num2.text)
-func sub(): num1 - num2
-func mul(): num1 * num2
-func div(): num1 / num2
-func power(): num1 ** num2
-
-
 func _on_btn_clear_pressed():
 	$txt_num1.text = ""
 	$txt_num2.text = ""
@@ -22,9 +16,26 @@ func _on_btn_exit_pressed():
 
 func _on_btn_add_pressed():	
 	getnums()
-	$output.text = 
-	return
-func _on_btn_sub_pressed(): pass
-func _on_btn_mul_pressed():  pass
-func _on_btn_mod_pressed(): pass
-func _on_btn_pow_pressed(): pass
+	var add = num1 + num2
+	$output.text = str(add)
+	
+func _on_btn_sub_pressed():
+	getnums()
+	var sub = num1 - num2
+	$output.text = str(sub)
+func _on_btn_mul_pressed():
+	getnums()
+	var mul = num1 * num2
+	$output.text = str(mul)
+func _on_btn_mod_pressed():
+	getnums()
+	var mod = num1 % num2
+	$output.text = str(mod)
+func _on_btn_pow_pressed():
+	getnums()
+	var pow = num1 ** num2
+	$output.text = str(pow)
+func _on_btn_div_pressed():
+	getnums()
+	var div = num1 / num2
+	$output.text = str(div)
