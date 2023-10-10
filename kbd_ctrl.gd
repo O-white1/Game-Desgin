fc8977491ffc697d6fac25367fa34667c7ac91e0
O-white1 <white.o3@stu.janesville.k12.wi.us>
2023-10-10 12:56:31 -0500

    10/10 game des final 12:56

--- kbd_ctrl.gd
+++ kbd_ctrl.gd
@@ -8,4 +8,24 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if dragging:
+		$draggable.global_position = get_global_mouse_position() - offset
+
+
+func _on_interact_mouse_entered():
+	print("Hover")
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		$interact.queue_free()
+	
+var dragging = false
+var offset = Vector2.ZERO
+
+
+func _on_button_button_down():
+	dragging = true
+	offset = get_global_mouse_position() - $draggable.global_position
+
+
+func _on_button_button_up():
+	dragging = false
+	
