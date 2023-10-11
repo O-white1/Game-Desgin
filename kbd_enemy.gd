dfcb3348e3bd0b5c692b9dc44ac21f08a6ec02a9
O-white1 <white.o3@stu.janesville.k12.wi.us>
2023-10-11 13:14:46 -0500

    10/11/23 1:14 gd final

---
+++ kbd_enemy.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+var speed = 100
+var direction = -1
+
+func _ready():
+	$Timer.start()
+func _physics_process(_delta):
+	self.linear_velocity.x = speed * direction
+
+func _on_timer_timeout():
+	direction *= -1
+
+
+func _on_body_entered(body):
+	if body.name == "kbd_player":
+		body.queue_free() #death
+		OS.alert("You Died!")
