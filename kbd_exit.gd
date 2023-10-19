638d80f16884a5d75a1946f7a5f21231e8556dcb
O-white1 <white.o3@stu.janesville.k12.wi.us>
2023-10-19 13:00:18 -0500

    12:59 final of GD

--- kbd_exit.gd
+++ kbd_exit.gd
@@ -1,5 +1,7 @@
 extends Area2D
-
+@export var next_level = ""
 func _on_body_entered(body):
 	if body.name == "kbd_player":
 		OS.alert("You Won!")
+		var lvl = "res://" + next_level + ".tscn"
+		get_tree().change_scene_to_file(lvl)
