9a80f2b0c9f49bffb63264411f6d9607323543a0
O-white1 <white.o3@stu.janesville.k12.wi.us>
2023-11-02 12:50:36 -0500

    12:50 11/2/2023

--- phys_demo_self.gd
+++ phys_demo_self.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Panel/gravSlide.value = $RigidBody2D.gravity_scale
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		var nball = preload("res://rigid_body_2d.tscn").instantiate()
@@ -17,24 +17,18 @@ func _input(event):
 		add_child(nball)
 
 
-func _on_grav_slide_value_changed(value):
-	for child in get_children():
-		if child is RigidBody2D:
-			child.gravity_scale = value
+	
+
 
 
 func _on_button_pressed():
+	var wind = int($"Debug panel/txt_wind".text)
+	var grav= int($"Debug panel/txt_gravity".text)
 	for child in get_children():
 		if child is RigidBody2D:
-			child.apply_central_impulse(Vector2.RIGHT * 500)
+			child.apply_central_impulse(Vector2.RIGHT * wind)
+			child.gravity_scale = grav
 
 
 func _on_button_2_pressed():
-	var v_intertia = float($Panel/txtI.text)
-	var v_x = float($Panel/txtVx.text)
-	var v_y = float($Panel/txtVy.text)
-	var velocity = Vector2(v_x, v_y)
-	for child in get_children():
-		if child is RigidBody2D:
-			child.intertia = v_intertia
-			child.linear_velocity = velocity
+	get_tree().reload_current_scene()
