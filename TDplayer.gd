bd2cd78510b2f853d8d0765d9c886251485079d2
O-white1 <white.o3@stu.janesville.k12.wi.us>
2023-11-07 12:56:04 -0600

    11/7 12:55
    Tile work was done on new top-down scene an animations are to be fleshed out.

--- TDplayer.gd
+++ TDplayer.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 150 # was 80
+const SPEED = 130 # was 80
 # const JUMP_VELOCITY = -400.0
 var inertia = Vector2()
 var look_direction = Vector2.DOWN
@@ -18,11 +18,11 @@ func _physics_process(delta):
 	var direction = Vector2(
 		Input.get_axis("ui_left", "ui_right"),
 		Input.get_axis("ui_up", "ui_down")
-	)
+	).normalized()
+	update_animation(direction)
 		
 	if direction.length() > 0:
 		look_direction = direction
-		direction = direction.normalized()
 		velocity = direction * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2(), SPEED) #velocity smoothly goes to 0
@@ -34,4 +34,21 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		menu_instance.show()
 		get_tree().paused = true
+		
 	
+func update_animation(direction):
+	var a_name = "idle_down"
+	if direction.length() > 0:
+		look_direction = direction
+		a_name = "walk_"
+		if direction.x != 0:
+			a_name +="side"
+			$AnimatedSprite2D.flip_h = direction.x < 0
+		elif direction.y < 0:
+			a_name += "up"
+		elif direction.y > 0:
+			a_name += "down"
+	else: pass
+	
+	if $AnimatedSprite2D.animation != a_name:
+		$AnimatedSprite2D.animation = a_name
